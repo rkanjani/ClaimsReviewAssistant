45ee535fb669f3b6dfb5da0ebfebed088d0cc250
Rohan Kanjani <rohankanjani1@gmail.com>
2026-02-19 11:17:27 -0500

    fixed deploy error

--- server/index.ts
+++ server/index.ts
@@ -566,7 +566,7 @@ const distPath = path.join(__dirname, '../dist');
 app.use(express.static(distPath));
 
 // SPA fallback - serve index.html for all non-API routes
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
